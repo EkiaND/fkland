0f91392555e60bce2698b513c15aefbc9f3c478b
Ekia OFF <velkyoofficiel@gmail.com>
2024-01-07 09:21:33 +0100

    Retrait de l'asynchronisation

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const axios = require('axios');
 exports.handler = async (event, context) => {
   const apiKey = "RGAPI-50e3a901-6822-487d-ad53-11165f65a852";
 
-  const getRank = async (summonerId) => {
+  const getRank = (summonerId) => {
     try {
-      const rankResponse = await axios.get(`https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${apiKey}`);
+      const rankResponse = axios.get(`https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${apiKey}`);
       const soloDuoRankInfo = rankResponse.data.find(queue => queue.queueType === 'RANKED_SOLO_5x5');
       return soloDuoRankInfo ? {
         tier: soloDuoRankInfo.tier,
@@ -38,25 +38,25 @@ exports.handler = async (event, context) => {
       'l Tennessee'
     ];
 
-    const summonerPromises = players.map(async (playerName) => {
+    for (const playerName of players) {
       const encodedName = encodeURIComponent(playerName);
-      const summonerData = await axios.get(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodedName}?api_key=${apiKey}`);
+      const summonerData = axios.get(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodedName}?api_key=${apiKey}`);
       const summonerId = summonerData.data.id;
 
-      const rankInfo = await getRank(summonerId);
+      // Utilisez l'ID d'invocateur pour obtenir les informations de rang.
+      const rankInfo = getRank(summonerId);
 
+      // Si rankInfo est null, cela signifie que le joueur n'a pas de classement en solo/duo ou qu'une erreur s'est produite.
       if (rankInfo) {
         playersData[playerName] = {
           summonerId: summonerId,
-          wins: rankInfo.wins,
-          losses: rankInfo.losses,
-          winrate: ((rankInfo.wins / (rankInfo.wins + rankInfo.losses)) * 100).toFixed(2),
-          rank: rankInfo,
+          wins: rankInfo.wins, // Nombre total de victoires en solo/duo queue.
+          losses: rankInfo.losses, // Nombre total de défaites en solo/duo queue.
+          winrate: ((rankInfo.wins / (rankInfo.wins + rankInfo.losses)) * 100).toFixed(2), // Calcul du taux de victoire.
+          rank: rankInfo, // Informations de classement en solo/duo queue.
         };
       }
-    });
-
-    await Promise.all(summonerPromises);
+    }
 
     return {
       statusCode: 200,
